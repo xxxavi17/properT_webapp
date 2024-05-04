1658614c100741ea1dd483c876ba3534ccabe360
xxxavi17 <xavier.f.machado@ua.pt>
2024-05-04 16:32:03 +0100

    footer

--- js/main.js
+++ js/main.js
@@ -1,19 +1,6 @@
 (function ($) {
     "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-
-
-
     // Sticky Navbar
     $(window).scroll(function () {
         if ($(this).scrollTop() > 45) {
