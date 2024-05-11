4b74dee99de0d740d7429cf2c0867a84882e7546
xxxavi17 <xavier.f.machado@ua.pt>
2024-05-12 00:24:32 +0100

    updates

--- js/index.js
+++ js/index.js
@@ -7,9 +7,9 @@ function searchProperties() {
     var normalizedInput = input.replace(/[^a-z0-9]+/gi, '');
 
     // Construa a URL baseada no input e tipos de transação e propriedade
-    if (normalizedInput.includes('lisboa') && transactionType === 'buy' && propertyType === 'house') {
+    if (normalizedInput.includes('lisboa') && transactionType === 'buy' && propertyType === 'apartment') {
         window.location.href = 'lisboa-comprar.html'; // Página específica para comprar casa em Lisboa
-    } else if (normalizedInput.includes('porto') && transactionType === 'rent' && propertyType === 'apartment') {
+    } else if (normalizedInput.includes('porto') && transactionType === 'rent' && propertyType === 'houset') {
         window.location.href = 'porto-arrendar.html'; // Página específica para alugar apartamento no Porto
     } else {
         // Redireciona para uma página de não encontrado se nenhum termo específico ou condição de transação for correspondida
@@ -27,9 +27,9 @@ function searchPropertiesAdmin() {
     var normalizedInput = input.replace(/[^a-z0-9]+/gi, '');
 
     // Construa a URL baseada no input e tipos de transação e propriedade
-    if (normalizedInput.includes('lisboa') && transactionType === 'buy' && propertyType === 'house') {
+    if (normalizedInput.includes('lisboa') && transactionType === 'buy' && propertyType === 'apartment') {
         window.location.href = 'lisboa-comprar-admin.html'; // Página específica para comprar casa em Lisboa
-    } else if (normalizedInput.includes('porto') && transactionType === 'rent' && propertyType === 'apartment') {
+    } else if (normalizedInput.includes('porto') && transactionType === 'rent' && propertyType === 'house') {
         window.location.href = 'porto-arrendar-admin.html'; // Página específica para alugar apartamento no Porto
     } else {
         // Redireciona para uma página de não encontrado se nenhum termo específico ou condição de transação for correspondida
