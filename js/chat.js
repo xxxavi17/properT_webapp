c3f7318eb6b25f93cd02ffcd192312cfe3d01b06
xxxavi17 <xavier.f.machado@ua.pt>
2024-05-21 16:22:17 +0100

    updates chat e navbar do staff

--- js/chat.js
+++ js/chat.js
@@ -22,3 +22,14 @@ var chatbox = document.getElementById('chatlogs');
             }
         });
 
+        document.getElementById('searchInput').addEventListener('keypress', function(event) {
+            if (event.key === 'Enter') {
+                const searchValue = event.target.value.toLowerCase();
+                if (searchValue === 'maria' || searchValue === 'maria teresa') {
+                    window.location.href = 'clients_teresa_only.html';
+                } else {
+                    alert('Client not found');
+                }
+            }
+        });
+
